c4fee6af63761b5f8c596b6ef6e8cf4f9e80a5b2
tabrisrp <tabrisrp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-24 20:55:32 +0000

    Version 1.2
    git-svn-id: https://plugins.svn.wordpress.org/wp-news-feed-widget/trunk@793198 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -2,10 +2,10 @@
 
 Contributors: tabrisrp
 Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=6V74BBTNMWW38&lc=FR&item_name=R%c3%a9my%20Perona&currency_code=EUR&bn=PP%2dDonationsBF%3abtn_donate_SM%2egif%3aNonHosted
-Tags: widget, news
+Tags: widget, news, pagination
 Requires at least: 3.2
-Tested up to: 3.6
-Stable tag: 1.1.1
+Tested up to: 3.7
+Stable tag: 1.2
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -15,9 +15,9 @@ A simple news feed widget with pagination
 
 WP News feed widget is a simple news feed widget, with pagination, to display the latest news on your website. It will display a list of your latest news titles, with the publication time (if today) or the publication date next to it.
 
-In the widget configuration, you can set the number of posts you want to include, the number of posts per page and disable the plugin's css if you want to apply your own.
+In the widget configuration, you can set the number of posts you want to include, the number of posts per page, chosse a style or disable the plugin's css if you want to apply your own.
 
-More features coming in future releases.
+The widget is customizable through a lot of filters and actions hooks.
 
 == Installation ==
 
@@ -29,30 +29,49 @@ More features coming in future releases.
 
 = What are the css classes to style the widget ? =
 
-`/* ul list wrapper */
-.wp-newsfw-list {}
+`
+/* Select category dropdown */
+    .wp-newsfw-select> .postform {}
+
+/* pager wrapper */
+.wpnewsfw-pager,
+.info_text {}
+
+/* pager items */
+.page_link {}
+
+.active_page {}
+
+.no_more {}
+
+/* prev & next items */
+.visually-hidden {}
+
+[dir=rtl] .visually-hidden {}
+
+.previous_link,
+.next_link {}
 
 /* list items */
 .wp-newsfw-item {}
-
-/* ul pager wrapper */
-.pager {}
-
-/* pager items */
-.pager a,
-.pager span {}
-
-/* previous page item */
-.prev {}
-
-/* next page item */
-.next {
-    margin-left: 5px;
-}`
+`
 
 = Why can't I configure more options ? =
 
-More configuration is coming in future releases.
+Options & customization can be added with the filter & hooks available, or with the add-ons.
+
+= List of filters & actions hooks =
+
+= Filters =
+* `wpnfw_query_args` (array) : Allows changing the main widget query args
+* `wpnfw_time_format` (string) : Allows changing the time format displayed in the widget
+* `wpnfw_date_format` (string) : Allows changing the date format displayed in the widget
+
+= Actions =
+* `wpnfw_before_link` : Runs before an item link output
+* `wpnfw_after_link` : Runs after an item link output
+* `wpnfw_before_title` : Runs before the title output (inside the link)
+* `wpnfw_after_title` : Runs after the title output (inside the link)
 
 == Screenshots ==
 
@@ -60,6 +79,11 @@ More configuration is coming in future releases.
 2. Widget display
 
 == Changelog ==
+
+= 1.2 =
+* Added filters and actions hooks to allow customization of the widget settings and output
+* New Pager script
+* New default styles for light and dark themes
 
 = 1.1.1 =
 * Security fixes
--- wp-news-feed-widget.php
+++ wp-news-feed-widget.php
@@ -3,12 +3,12 @@
 Plugin Name: WP News Feed Widget
 Plugin URI: http://remyperona.fr/wp-news-feed-widget/
 Description: A news feed widget with pagination
-Version: 1.1.1
+Version: 1.2
 Author: Rémy Perona
 Author URI: http://remyperona.fr
 Author Email: remperona@gmail.com
-Text Domain: wp-newsfw
-Domain Path: /lang/
+Text Domain: wp-news-feed-widget
+Domain Path: /lang
 Network: true
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
@@ -29,7 +29,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-class RP_Wpnwf extends WP_Widget {
+class RP_Wpnfw extends WP_Widget {
 
 	/*--------------------------------------------------*/
 	/* Constructor
@@ -41,13 +41,17 @@ class RP_Wpnwf extends WP_Widget {
 	 */
 	public function __construct() {
 
+        add_action( 'plugins_loaded', array( $this, 'wp_newsfw_textdomain' ) );
+        // AJAX handler
+        add_action( 'wp_ajax_get_widget_posts', array( $this, 'get_widget_posts' ) );
+        add_action( 'wp_ajax_nopriv_get_widget_posts', array( $this, 'get_widget_posts' ) );
 
 		parent::__construct(
-			'wp-newsfw',
-			__( 'WP News Feed Widget', 'wp-newsfw' ),
+			'wp-news-feed-widget',
+			__( 'WP News Feed Widget', 'wp-news-feed-widget' ),
 			array(
-				'classname'		=>	'wp-newsfw',
-				'description'	=>	__( 'A news feed widget with pagination', 'wp-newsfw' )
+				'classname'		=>	'wp-news-feed-widget',
+				'description'	=>	__( 'A news feed widget with pagination', 'wp-news-feed-widget' )
 			)
 		);
 
@@ -74,8 +78,10 @@ class RP_Wpnwf extends WP_Widget {
 		echo $before_widget;
 
 		$title = empty( $instance['title'] ) ? '' : apply_filters( 'widget_title', $instance['title'] );
-		$numberposts = empty( $instance['numberposts'] ) ? '' : apply_filters( 'numberposts', $instance['numberposts'] );
-		$posts_per_page = empty( $instance['posts_per_page'] ) ? '' : apply_filters( 'posts_per_page', $instance['posts_per_page'] );
+		$numberposts = empty( $instance['numberposts'] ) ? '' : $instance['numberposts'];
+		$posts_per_page = empty( $instance['posts_per_page'] ) ? '' : $instance['posts_per_page'];
+		$truncate = empty( $instance['truncate'] ) ? '' : $instance['truncate'];
+		$title_limit = empty( $instance['limit'] ) ? '' : $instance['limit'];
 
 		include( plugin_dir_path( __FILE__ ) . '/views/widget.php' );
 
@@ -93,10 +99,13 @@ class RP_Wpnwf extends WP_Widget {
 
 		$instance = $old_instance;
 
-		$instance['title'] = sanitize_text_field( $new_instance['title'] );
-		$instance['numberposts'] = sanitize_text_field( $new_instance['numberposts'] );
-		$instance['posts_per_page'] = sanitize_text_field( $new_instance['posts_per_page'] );
-		$instance['css_active'] = sanitize_text_field( $new_instance['css_active'] );
+		$instance['title'] = strip_tags( stripslashes( $new_instance['title'] ) );
+		$instance['numberposts'] = strip_tags( stripslashes( $new_instance['numberposts'] ) );
+		$instance['posts_per_page'] = strip_tags( stripslashes( $new_instance['posts_per_page'] ) );
+		$instance['truncate'] = strip_tags( stripslashes( $new_instance['truncate'] ) );
+		$instance['limit'] = strip_tags( stripslashes( $new_instance['limit'] ) );
+		$instance['css_active'] = strip_tags( stripslashes( $new_instance['css_active'] ) );
+        $instance['css_style'] = strip_tags( stripslashes( $new_instance['css_style'] ) );
 
 		return $instance;
 
@@ -108,21 +117,28 @@ class RP_Wpnwf extends WP_Widget {
 	 * @param	array	instance	The array of keys and values for the widget.
 	 */
 	public function form( $instance ) {
+        $instance_args = array(
+			    'title' => __( 'News feed', 'wp-news-feed-widget' ),
+			    'numberposts' => 50,
+			    'posts_per_page' => 10,
+			    'truncate' => 0,
+			    'limit' => 30,
+			    'css_active' => 1,
+			    'css_style' => 'light'
+			);
 
 		$instance = wp_parse_args(
 			(array) $instance,
-			array(
-			    'title' => __( 'News feed', 'wp-newsfw' ),
-			    'numberposts' => 50,
-			    'posts_per_page' => 10,
-			    'css_active' => 1
-			)
+			$instance_args
 		);
 
 		$title = esc_attr( $instance['title'] );
 		$numberposts = esc_attr( $instance['numberposts'] );
 		$posts_per_page = esc_attr( $instance['posts_per_page'] );
+		$truncate = esc_attr( $instance['truncate'] );
+		$title_limit = esc_attr( $instance['limit'] );
 		$css_active = esc_attr( $instance['css_active'] );
+		$css_style = esc_attr( $instance['css_style'] );
 
 		// Display the admin form
 		include( plugin_dir_path(__FILE__) . '/views/admin.php' );
@@ -133,13 +149,17 @@ class RP_Wpnwf extends WP_Widget {
 	/* Public Functions
 	/*--------------------------------------------------*/
 
+	public function wp_newsfw_textdomain() {
+        load_plugin_textdomain( 'wp-news-feed-widget', false, dirname( plugin_basename( __FILE__ ) ) . '/lang/' );
+    }
+
 	/**
 	 * Registers and enqueues widget-specific styles.
 	 */
 	public function register_widget_styles() {
 	    $css_active = $this->widget_options( 'css_active' );
 	    if ( $css_active == 1 ) {
-		    wp_enqueue_style( 'wp-newsfw-widget-styles', plugins_url( 'wp-news-feed-widget/css/wp-newsfw.css' ) );
+		    wp_enqueue_style( 'wp-newsfw-widget-styles', plugins_url( 'wp-news-feed-widget/css/' . $this->widget_options( 'css_style' ) . '.css' ) );
 		}
 
 	} // end register_widget_styles
@@ -148,21 +168,107 @@ class RP_Wpnwf extends WP_Widget {
 	 * Registers and enqueues widget-specific scripts.
 	 */
 	public function register_widget_scripts() {
-		wp_enqueue_script( 'wp-newsfw-script', plugins_url( 'wp-news-feed-widget/js/wp-newsfw.min.js' ), array('jquery') );
-		wp_localize_script( 'wp-newsfw-script', 'pager', array( 'perpage' => $this->widget_options( 'posts_per_page' ) ) );
+		wp_enqueue_script( 'wp-newsfw-script', plugins_url( 'wp-news-feed-widget/js/wp-newsfw.min.js' ), array( 'jquery' ), '1.2', true );
+		wp_localize_script( 'wp-newsfw-script', 'wpnewsfw', array( 'prev' => '<span class="visually-hidden">' . __( 'Prev', 'wp-news-feed-widget' ) . '</span>', 'next' => '<span class="visually-hidden">' . __( 'Next', 'wp-news-feed-widget' ) . '</span>', 'perpage' => $this->widget_options( 'posts_per_page' ), 'numberposts' => $this->widget_options( 'numberposts' ), 'truncate' => $this->widget_options( 'truncate' ), 'title_limit' => $this->widget_options( 'limit' ), 'ajaxurl' => admin_url( '/admin-ajax.php' ), 'security' => wp_create_nonce( 'wpnewsfw-security' ) ) );
 
 	} // end register_widget_scripts
 
+    /**
+	 * Retrieves the widget settings values
+	 */
     public function widget_options( $key ) {
         $widget_id = $this->number;
-	    $wp_newsfw_data = get_option( 'widget_wp-newsfw' );
+	    $wp_newsfw_data = $this->get_settings();
 	    return $wp_newsfw_data[$widget_id][$key];
+    }
+
+    /**
+	 * Outputs the widget list
+	 */
+	 public function the_feed_list( $cat = '', $numberposts, $truncate, $title_limit) {
+        $html;
+        $cat = absint( $cat );
+        $html = $cat;
+        $numberposts = ( int ) $numberposts;
+        $truncate = absint( $truncate );
+        $title_limit = ( int ) $title_limit;
+        $query_args = array(
+            'post_status' => 'publish',
+            'posts_per_page' => $numberposts,
+            'ignore_sticky_posts' => 1,
+        );
+        if ( !empty( $cat ) ) {
+            $query_args['tax_query'] = array(
+                array(
+                    'taxonomy' => 'category',
+                    'field' => 'id',
+                    'terms' => $cat
+                )
+            );
+        }
+        $query_args = apply_filters( 'wpnfw_query_args', $query_args );
+        $posts = new WP_Query( $query_args );
+        // if we have results
+        if ( $posts->have_posts() ) {
+            $html = '';
+            while ( $posts->have_posts() ) {
+                $posts->the_post();
+                $html .= '<li class="wp-newsfw-item">';
+                if ( get_the_date( 'm d Y' ) == date( 'm d Y' ) ) { 
+                    $html .= '<time datetime="' . get_the_time( 'H:i' ) . '">' . apply_filters( 'wpnfw_time_format', get_the_time( get_option( 'time_format') ) ) . '</time> ';
+                } else {
+                    $html .= '<time datetime="' . get_the_date( 'Y-m-d' ) . '">' . apply_filters( 'wpnfw_date_format', get_the_date( get_option( 'date_format') ) ) . '</time> ';
+                }
+                $html .= do_action( 'wpnfw_before_link', get_the_id() );
+                $html .= '<a href="' . get_permalink() . '" title="' . the_title_attribute( 'echo=0' ) . '">';
+                $html .= do_action( 'wpnfw_before_title', get_the_id() );
+                if ( $truncate == 1 ) {
+                    $html .= $this->truncate_title( $title_limit );
+                } else {
+                    $html .= get_the_title();
+                }
+                $html .= do_action( 'wpnfw_after_title', get_the_id() );
+                $html .= '</a>';
+                $html .= do_action( 'wpnfw_after_link', get_the_id() );
+                
+                $html .= '</li>';
+            }
+            wp_reset_postdata();
+        } else {
+            $html = __( 'No posts for this category', 'wp-news-feed-widget' );
+        }
+        return $html;
+	 }
+
+    /**
+	 * Handler function for AJAX request
+	 */
+    public function get_widget_posts() {
+        $html = '';
+        if ( isset( $_POST['idCat'] ) && isset( $_POST['security'] ) ) {
+            if (check_ajax_referer( 'wpnewsfw-security', 'security', false ) ) {
+                $html = $this->the_feed_list( $_POST['idCat'], $_POST['numberposts'], $_POST['truncate'], $_POST['title_limit'] );
+            } else {
+                $html = __( 'Something not okay here', 'wp-news-feed-widget' );
+            }
+        } else {
+            $html = __( 'No category selected', 'wp-news-feed-widget' );
+        }
+        echo json_encode( $html );
+        die();
+    }
+
+    /**
+	 * Truncates item's title by the $limit defined in the widget settings
+	 */
+    public function truncate_title( $limit ) {
+        $the_title = get_the_title();
+        if ( mb_strlen( $the_title, 'utf8' ) > $limit ) {
+            $last_space = strrpos( substr( $the_title, 0, $limit ), ' ' );
+            $the_title = substr( $the_title, 0, $last_space ) . '...';
+        }
+        return $the_title;
     }
 } // end class
 
-add_action( 'widgets_init', create_function( '', 'register_widget("RP_Wpnwf");' ) );
-
-function wp_newsfw_textdomain() {
-    load_plugin_textdomain( 'wp-newsfw', false, dirname( plugin_basename( __FILE__ ) ) . '/lang/' );
-}
-add_action( 'plugins_loaded', 'wp_newsfw_textdomain' );
+add_action( 'widgets_init', create_function( '', 'register_widget("RP_Wpnfw");' ) );
